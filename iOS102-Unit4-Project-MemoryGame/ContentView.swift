2772955449c947478b6b6f4998fbe77254c7d61a
Raymond Chen <ahrchen@gmail.com>
2024-07-19 16:08:15 -0700

    Initial Commit

---
+++ iOS102-Unit4-Project-MemoryGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS102-Unit4-Project-MemoryGame
+//
+//  Created by Raymond Chen on 7/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS102-Unit4-Project-MemoryGame/iOS102_Unit4_Project_MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS102_Unit4_Project_MemoryGameApp.swift
+//  iOS102-Unit4-Project-MemoryGame
+//
+//  Created by Raymond Chen on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS102_Unit4_Project_MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
